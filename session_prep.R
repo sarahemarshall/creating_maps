dd2062b7acf9150265329b03c47c4fe057261994
Sarah Marshall <42128033+sarahemarshall@users.noreply.github.com>
2018-09-12 11:18:55 +1200

    Added shp file and updated Rmd

--- session_prep.R
+++ session_prep.R
@@ -1,7 +1,7 @@
 # session_prep.R 
 # Installs R packages required for this introduction to making maps in R.
 
-requiredPackages <- c("knitr",  "tidyverse","ggplot2", "maps", 
+requiredPackages <- c("knitr",  "tidyverse", "magrittr", "ggplot2", "maps", 
                       "maptools", "rgdal", "rgeos", 
                       "sp", "ggplot2", "leaflet", "readxl", "readr", 
                       "ggmap",  "tmap", "tmaptools" )
@@ -10,7 +10,7 @@ requiredPackages <- c("knitr",  "tidyverse","ggplot2", "maps",
 install.packages(requiredPackages, dependencies=TRUE)
 
 
-# OR install packages not on system (not recommended as may have version issues)
+# Install packages not on system (not recommended as may have version issues)
 # packages_to_install <- requiredPackages[!(requiredPackages %in% installed.packages()[,1])]
 # if(length(packages_to_install)>0 ) install.packages(packages_to_install, dependencies=TRUE)
 
